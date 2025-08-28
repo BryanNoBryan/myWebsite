97efc524602d0f4644c222b98bf61533c4569fc3
Bryan Liu <121914672+BryanNoBryan@users.noreply.github.com>
2025-08-27 23:14:36 -0400

    'yes'

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
+import dotenv from 'dotenv';
+dotenv.config();
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() },
-                runes: true
+const config = { 
+    kit: { 
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+        }
+
+     },
+    runes: true
 };
 
 export default config;
