7a8a2e85fe6fb3e511184816fc5deee74cecea26
Bryan Liu <121914672+BryanNoBryan@users.noreply.github.com>
2025-08-27 23:21:04 -0400

    'yes'

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
             fallback: '404.html'
         }),
         paths: {
-            base: process.argv.includes('dev') ? '' : 'myWebsite'
+            base: process.argv.includes('dev') ? '' : '/myWebsite'
         }
 
      },
