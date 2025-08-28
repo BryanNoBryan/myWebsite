5cfef7ad154a247216133f2dc5975d74686bd882
Bryan Liu <121914672+BryanNoBryan@users.noreply.github.com>
2025-08-28 01:12:03 -0400

    'pls'

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
-const basePath = process.argv.includes('dev') ? '' : '/myWebsite/';
+const basePath = process.argv.includes('dev') ? '' : '/myWebsite';
 console.log('✅ Base path resolved to:', basePath);
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { 
+const config = {
+    
     kit: { 
         adapter: adapter({
             fallback: '404.html'
         }),
         paths: {
+            relative: false,
             base: process.argv.includes('dev') ? '' : '/myWebsite'
         }
 
