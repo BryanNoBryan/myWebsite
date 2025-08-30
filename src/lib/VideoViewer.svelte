ed9aa1d45b027d66b71c6c1b8a690b0c0cfb0042
Bryan Liu <121914672+BryanNoBryan@users.noreply.github.com>
2025-08-30 03:19:15 -0400

    MUST ADD SOURCE AND TYPE B/C on mobile the element is Content-Type: text/html which prevents rendering

--- src/lib/VideoViewer.svelte
+++ src/lib/VideoViewer.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <video controls playsinline loop>
-  <source {src} type="video/mp4" />
+  <source {src} type="video/mp4" /> 
+  <!-- MUST ADD SOURCE AND TYPE B/C on mobile the element is Content-Type: text/html which prevents rendering -->
   <track kind="captions" />
 </video>
