7e3ca72d94ae4004f7ad18df20a3398cc75ba51a
Bryan Liu <121914672+BryanNoBryan@users.noreply.github.com>
2025-10-07 00:55:58 -0400

    fixed a link

---
+++ docs/_app/immutable/chunks/BM2pZCoC.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1whot7h?.base??"",t=globalThis.__sveltekit_1whot7h?.assets??s;export{t as a,s as b};
--- docs/_app/immutable/chunks/ByG7FEK3.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_fkcvi6?.base??"",a=globalThis.__sveltekit_fkcvi6?.assets??s;export{a,s as b};
